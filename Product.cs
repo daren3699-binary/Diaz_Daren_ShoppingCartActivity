8205d01a28ea997a692c6ff3b870c9eadada36d2
diaz_daren3699-binary <darendiaz1214@gmail.com>
2026-04-13 21:33:51 +0800

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace v1_DIAZ_DAREN_V_SHOPPINGCARTACTIVTY
+{
+    class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int RemainingStock;
+
+        public void DisplayProduct()
+        {
+          Console.WriteLine($"{Id}. {Name} - PHP{Price} (Stock: {RemainingStock}) ");
+        }
+        /*
+        public double GetItemTotal(int quantity)
+        {
+
+        }
+
+        public bool HasEnoughStock(int quantity)
+        {
+
+        }
+
+        public void DeductStock(int quantity)
+        {
+
+        }*/
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace v1_DIAZ_DAREN_V_SHOPPINGCARTACTIVTY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initial product array
+            Product[] products =
+            {
+                new Product {Id = 1,
+                             Name = "Pencil",
+                             Price = 6.00f,
+                             RemainingStock = 30,},
+
+                new Product {Id = 2,
+                             Name = "Pad Paper",
+                             Price = 20.00f,
+                             RemainingStock = 17,},
+
+                new Product {Id = 3,
+                             Name = "Black Ballpen",
+                             Price = 9.00f,
+                             RemainingStock = 20,},
+
+                new Product {Id = 4,
+                             Name = "Correction Tape",
+                             Price = 28.00f,
+                             RemainingStock = 10,},
+
+                new Product {Id = 5,
+                             Name = "Whiteboard Marker",
+                             Price = 12.00f,
+                             RemainingStock = 21,},
+            };
+
+            Console.WriteLine("-----STORE MENU-----");
+            foreach (Product product in products)
+            {
+                product.DisplayProduct();
+            }
+
+
+        }
+    }
+}
